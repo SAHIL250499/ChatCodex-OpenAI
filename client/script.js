04596646e584bfc6b31bf690ebce99a629199083
SAHIL250499 <samp9270@gmail.com>
2023-02-05 23:31:41 +0530

    updated server url

--- client/script.js
+++ client/script.js
@@ -76,7 +76,7 @@ const handleSubmit=async (e)=>{
 
   //fetch data from server->bots response
 
-  const response=await fetch('http://localhost:5000',{
+  const response=await fetch('https://chatcodex-4ex6.onrender.com',{
     method:'POST',
     headers:{
       'Content-Type':'application/json'
